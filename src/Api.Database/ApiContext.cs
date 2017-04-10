bb3ec08c473ac4ff4e9aba5b04f91a5d5b75440f
Gary Woodfine <gary.woodfine@threenine.co.uk>
2017-04-10 18:33:53 +0100

    working sample

---
+++ src/Api.Database/ApiContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Database
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext (DbContextOptions<ApiContext> options) : base(options){ }
+    }
+}
