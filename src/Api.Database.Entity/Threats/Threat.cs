4eaa208d8fd9c4d3b829f4d1fce06e55f36f6632
Gary Woodfine <gary.woodfine@threenine.co.uk>
2017-04-12 15:18:15 +0100

    Added omitted Threat

---
+++ src/Api.Database.Entity/Threats/Threat.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Portal.Database.Entity.Threats
+{
+    
+    public class Threat : BaseEntity
+    {
+
+        [Required]
+        [StringLength(100)]
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        [StringLength(255)]
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        [StringLength(255)]
+        [DataType(DataType.Text)]
+        public string Referer
+        {
+            get;
+            set;
+        }
+
+        public string Host
+        {
+            get;
+            set;
+        }
+
+        public string UserAgent
+        {
+            get;
+            set;
+        }
+       
+        public string XForwardHost { get; set; }
+
+        public string XForwardProto { get; set; }
+
+        public string QueryString { get; set; }
+
+        public string Protocol { get; set; }
+
+        [ForeignKey("Status")]
+        public int StatusId { get; set; }
+
+        [ForeignKey("Type")]
+        public int TypeId { get; set; }
+
+        public virtual ThreatType Type { get; set; }
+        public virtual Status Status { get; set; }
+
+
+    }
+}
