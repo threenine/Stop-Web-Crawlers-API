7de3237a0ac0779a3c27eba250dd184bc191c6ce
Gary Woodfine <gary.woodfine@threenine.co.uk>
2018-01-26 13:05:19 +0000

    Added example of Repository Query

--- src/Swc.Service/ReferrerService.cs
+++ src/Swc.Service/ReferrerService.cs
@@ -11,20 +11,31 @@ namespace Swc.Service
     public class ReferrerService : IReferrerService
     {
         private readonly IRepository<Threat> _refererRepository;
+        private readonly IRepository<Status> _statusRepository;
+        private readonly IRepository<ThreatType> _typeRepository;
 
-        public ReferrerService(IRepository<Threat> repository)
+        public ReferrerService(IRepository<Threat> repository, IRepository<Status> statusRepository, IRepository<ThreatType> typeRepository)
         {
             _refererRepository = repository;
+            _statusRepository = statusRepository;
+            _typeRepository = typeRepository;
         }
         public IEnumerable<Referer> GetAll()
         {
-            var threats = _refererRepository.Get();
+
+            var threats = _refererRepository.Get()
+                .Join(_statusRepository.Get(), t => t.StatusId, s => s.Id, (t, s) => new {t, s})
+                .Join(_typeRepository.Get(), tt => tt.t.TypeId, type => type.Id,
+                    (t1, type) =>
+                        new Threat {Name = @t1.t.Name, Referer = @t1.t.Referer, Type = type, Status = @t1.s});
+               
+                
             if (threats != null)
-            {
+             {
                 return Mapper.Map<IEnumerable<Referer>>(threats);
             }
 
-            return null;
+            return default(IEnumerable<Referer>);
         }
     }
 }
